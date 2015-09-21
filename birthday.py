2df19074505eadd00840a5a247a51f2bbf240cc9
jasminelou <yajielou@hanovernorwichschools.org>
2015-09-21 14:54:54 -0400

    Updated from Brython Server: 9/21/2015 2:54:54 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jasmine Lou
+Credit: The internet and classmates, and teachers
 Assignment:
 
 Your program will ask the user the following questions, in this order:
@@ -31,3 +31,39 @@ Example Session
   And the day? 11
   Eric, you are a fall baby of the stone age.
 """
+
+name = input("Hello, what is your name? ")
+month = input("Hi "+ name + ", what was the name of the month you were born in? ")
+month = month.lower()
+year = input("And what year were you born in, " + name + "? ")
+day = input("And the day? ")
+
+if month == "september" and day == "22":
+    print("Happy birthday!")
+
+if month == "october" and day == "31":
+    print("You were born on Halloween!")
+elif int(year) < int(1900) and month == "september":
+    print("You are a autumn baby of before 1900.")
+elif int(year) < int(1900) and month == "october":
+    print("You are a autumn baby of before 1900.")
+elif int(year) < int(1900) and month == "november":
+    print("You are a autumn baby of before 1900.")
+elif int(year) < int(1900) and month == "december":
+    print("You are a winter baby of before 1900.")
+elif int(year) < int(1900) and month == "january":
+    print("You are a winter baby of before 1900.")
+elif int(year) < int(1900) and month == "february":
+    print("You are a winter baby of before 1900.")
+elif int(year) < int(1900) and month == "march":
+    print("You are a spring baby of before 1900.")
+elif int(year) < int(1900) and month == "april":
+    print("You are a spring baby of before 1900.")
+elif int(year) < int(1900) and month == "may":
+    print("You are a spring baby of before 1900.")
+elif int(year) < int(1900) and month == "june":
+    print("You are a summer baby of before 1900.")
+elif int(year) < int(1900) and month == "july":
+    print("You are a summer baby of before 1900.")
+elif int(year) < int(1900) and month == "august":
+    print("You are a summer baby of before 1900.")
